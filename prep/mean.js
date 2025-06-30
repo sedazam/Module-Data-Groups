261093bdc979ff9cf2414c0f76495ca9aaacc381
sedazam <sedazam@gmail.com>
2025-06-30 18:45:30 +0100

    new file:   package-lock.json 	new file:   package.json 	modified:   prep/mean.js

--- prep/mean.js
+++ prep/mean.js
@@ -1,6 +1,6 @@
 function calculateMean(numbers) {
   if (numbers.length === 0) {
-    return 0; // Return 0 for an empty list
+    return 0; // Return // 0 for an empty list
   }
   const sum = numbers.reduce((acc, num) => acc + num, 0);
   return sum / numbers.length;
