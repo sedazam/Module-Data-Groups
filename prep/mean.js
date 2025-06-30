904337347f478a86267843987425fc1e8c9c1868
sedazam <sedazam@gmail.com>
2025-06-30 18:35:53 +0100

    modified:   prep/mean.js 	modified:   prep/mean.test.js

---
+++ prep/mean.js
@@ -0,0 +1,7 @@
+function calculateMean(numbers) {
+  if (numbers.length === 0) {
+    return 0; // Return 0 for an empty list
+  }
+  const sum = numbers.reduce((acc, num) => acc + num, 0);
+  return sum / numbers.length;
+}
