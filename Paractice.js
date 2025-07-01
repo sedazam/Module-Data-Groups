9dd69e1cbdec2142512dbc4222bfce969bcc5324
sedazam <sedazam@gmail.com>
2025-07-01 07:21:31 +0100

    new file:   Paractice.js 	deleted:    mean.js 	deleted:    mean.test.js 	modified:   prep/mean.js 	modified:   prep/mean.test.js

---
+++ Paractice.js
@@ -0,0 +1,5 @@
+// Write a function that takes an array of numbers and returns a new array with each number doubled.
+function doubleArray(numbers) {
+  return numbers.map((num) => num * 2);
+}
+console.log(doubleArray([1, 2, 3])); // [2, 4, 6]
--- prep/mean.js
+++ prep/mean.js
@@ -1,7 +1,7 @@
 function calculateMean(numbers) {
-  if (numbers.length === 0) {
-    return 0; // Return // 0 for an empty list
-  }
-  const sum = numbers.reduce((acc, num) => acc + num, 0);
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((acc, val) => acc + val, 0);
   return sum / numbers.length;
 }
+
+module.exports = calculateMean;
