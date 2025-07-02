7190331d48f09cb3357f01cc8389576efd28acb4
sedazam <sedazam@gmail.com>
2025-07-02 23:31:20 +0100

    modified:   prep/mean.test.js

--- prep/mean.test.js
+++ prep/mean.test.js
@@ -1,9 +1,7 @@
-const calculateMean = require("./mean");
-
 test("calculates the mean of a list of numbers", () => {
   const list = [3, 50, 7];
   const currentOutput = calculateMean(list);
   const targetOutput = 20;
 
-  expect(currentOutput).toEqual(targetOutput);
+  expect(currentOutput).toEqual(targetOutput); // 20 is (3 + 50 + 7) / 3
 });
