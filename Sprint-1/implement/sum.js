f4fe784a583f33c7f03e72dd8fd0553f849a1a98
sedazam <sedazam@gmail.com>
2025-07-04 06:16:23 +0100

    modified:   Sprint-1/implement/sum.js 	modified:   Sprint-1/implement/sum.test.js

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,7 @@
 function sum(elements) {
+  return elements
+    .filter((element) => typeof element === "number") // Filter out non-numerical elements
+    .reduce((accumulator, currentValue) => accumulator + currentValue, 0); // Sum the numerical elements
 }
 
 module.exports = sum;
--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,24 +13,55 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
 
-// Given an array with just one number
-// When passed to the sum function
-// Then it should return that number
+describe("sum function", () => {
+  test("given an empty array, returns 0", () => {
+    expect(sum([])).toBe(0);
+    expect(sum([null, undefined])).toBe(0);
+  });
 
-// Given an array containing negative numbers
-// When passed to the sum function
-// Then it should still return the correct total sum
+  // Given an array with just one number
+  // When passed to the sum function
+  // Then it should return that number
 
-// Given an array with decimal/float numbers
-// When passed to the sum function
-// Then it should return the correct total sum
+  test("given an array with one number, returns that number", () => {
+    expect(sum([42])).toBe(42);
+    expect(sum([-10])).toBe(-10);
+  });
 
-// Given an array containing non-number values
-// When passed to the sum function
-// Then it should ignore the non-numerical values and return the sum of the numerical elements
+  // Given an array containing negative numbers
+  // When passed to the sum function
+  // Then it should still return the correct total sum
 
-// Given an array with only non-number values
-// When passed to the sum function
-// Then it should return the least surprising value given how it behaves for all other inputs
+  test("given an array with negative numbers, returns the correct total sum", () => {
+    expect(sum([-10, -20, -30])).toBe(-60);
+    expect(sum([-10, 20, -30])).toBe(-20);
+  });
+
+  // Given an array with decimal/float numbers
+  // When passed to the sum function
+  // Then it should return the correct total sum
+
+  test("given an array with decimal numbers, returns the correct total sum", () => {
+    expect(sum([1.5, 2.5, 3.0])).toBe(7.0);
+    expect(sum([1.1, 2.2, -3.3])).toBeCloseTo(0.0);
+  });
+
+  // Given an array containing non-number values
+  // When passed to the sum function
+  // Then it should ignore the non-numerical values and return the sum of the numerical elements
+
+  test("given an array with non-number values, ignores them and returns the sum of numerical elements", () => {
+    expect(sum([10, "hello", 20, null, 30])).toBe(60);
+    expect(sum(["text", undefined, 5, 15])).toBe(20);
+  });
+
+  // Given an array with only non-number values
+  // When passed to the sum function
+  // Then it should return the least surprising value given how it behaves for all other inputs
+
+  test("given an array with only non-number values, returns 0", () => {
+    expect(sum(["hello", null, undefined])).toBe(0);
+    expect(sum([])).toBe(0);
+  });
+});
