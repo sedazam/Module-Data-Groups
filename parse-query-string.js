4f0d5cff52ca575aa0ec331594429d8e186e92ae
sedazam <sedazam@gmail.com>
2025-07-05 07:07:30 +0100

    modified:   parse-query-string.js 	modified:   parse-query-string.test.js

--- parse-query-string.js
+++ parse-query-string.js
@@ -1,7 +1,18 @@
+// function parseQueryString(queryString) {
+//   console.log(queryString);
+//   if (queryString === "") {
+//     return {}; // If the query string is empty, return an empty object.
+//   }
+//   return {}; // Placeholder for future implementation. Prevents retuning undefined. for non-empty query strings.
+// module.exports = parseQueryString;
 function parseQueryString(queryString) {
-  console.log(queryString);
-  if (queryString === "") {
-    return {}; // If the query string is empty, return an empty object.
-  }
-  return {}; // Placeholder for future implementation. Prevents retuning undefined. for non-empty query strings.
+  const queryParams = {};
+
+  const keyValuePair = queryString.split("=");
+  const key = keyValuePair[0]; // key will hold 'fruit'
+  const value = keyValuePair[1]; // value will hold 'banana'
+  queryParams[key] = value;
+
+  return queryParams;
+}
 module.exports = parseQueryString;
--- parse-query-string.test.js
+++ parse-query-string.test.js
@@ -1,9 +1,20 @@
+// const parseQueryString = require("./parse-query-string");
+// describe("", () => {
+//   test("given a query string with no query parameters, returns an empty object", function () {
+//     const input = "";
+//     const currentOutput = parseQueryString(input);
+//     const targetOutput = {};
+
+//     expect(currentOutput).toEqual(targetOutput);
+//   });
+// });
+
 const parseQueryString = require("./parse-query-string");
 describe("", () => {
-  test("given a query string with no query parameters, returns an empty object", function () {
-    const input = "";
+  test("given a query string with one pair of query params, returns them in object form", function () {
+    const input = "fruit=banana";
     const currentOutput = parseQueryString(input);
-    const targetOutput = {};
+    const targetOutput = { fruit: "banana" };
 
     expect(currentOutput).toEqual(targetOutput);
   });
