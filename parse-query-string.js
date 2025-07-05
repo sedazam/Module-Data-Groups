d9d9836e3cda6dc3646fffe238f898699836f3d9
sedazam <sedazam@gmail.com>
2025-07-05 07:22:02 +0100

    modified:   parse-query-string.js 	modified:   parse-query-string.test.js

--- parse-query-string.js
+++ parse-query-string.js
@@ -5,13 +5,23 @@
 //   }
 //   return {}; // Placeholder for future implementation. Prevents retuning undefined. for non-empty query strings.
 // module.exports = parseQueryString;
+// function parseQueryString(queryString) {
+//   const queryParams = {};
+
+//   const keyValuePair = queryString.split("=");
+//   const key = keyValuePair[0]; // key will hold 'fruit'
+//   const value = keyValuePair[1]; // value will hold 'banana'
+//   queryParams[key] = value;
+
+//   return queryParams;
+// }
+// module.exports = parseQueryString;
+
 function parseQueryString(queryString) {
   const queryParams = {};
 
-  const keyValuePair = queryString.split("=");
-  const key = keyValuePair[0]; // key will hold 'fruit'
-  const value = keyValuePair[1]; // value will hold 'banana'
-  queryParams[key] = value;
+  const [key, value] = queryString.split("="); // will hold ['fruit', 'banana']
+  queryParams[key] = value; // will set the property name with the value of the key variable
 
   return queryParams;
 }
--- parse-query-string.test.js
+++ parse-query-string.test.js
@@ -9,9 +9,30 @@
 //   });
 // });
 
+// const parseQueryString = require("./parse-query-string");
+// describe("", () => {
+//   test("given a query string with one pair of query params, returns them in object form", function () {
+//     const input = "fruit=banana";
+//     const currentOutput = parseQueryString(input);
+//     const targetOutput = { fruit: "banana" };
+
+//     expect(currentOutput).toEqual(targetOutput);
+//   });
+// });
+
 const parseQueryString = require("./parse-query-string");
-describe("", () => {
-  test("given a query string with one pair of query params, returns them in object form", function () {
+describe("parseQueryString()", () => {
+  test("given a queryString with no query parameters, returns an empty object", function () {
+    const input = "";
+    const currentOutput = parseQueryString(input);
+    const targetOutput = {};
+
+    expect(currentOutput).toEqual(targetOutput);
+  });
+});
+
+describe("parseQueryString()", () => {
+  test("given a queryString with one pair of query params, returns them in object form", function () {
     const input = "fruit=banana";
     const currentOutput = parseQueryString(input);
     const targetOutput = { fruit: "banana" };
