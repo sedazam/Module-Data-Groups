ea0cefb1f8866814fa73dcf23d3ea83b8b5c3502
sedazam <sedazam@gmail.com>
2025-07-05 06:02:49 +0100

    modified:   parse-query-string.js

--- parse-query-string.js
+++ parse-query-string.js
@@ -1,7 +1,7 @@
 function parseQueryString(queryString) {
   console.log(queryString);
   if (queryString === "") {
-    return {};
+    return {}; // If the query string is empty, return an empty object.
   }
-}
+  return {}; // Placeholder for future implementation. Prevents retuning undefined. for non-empty query strings.
 module.exports = parseQueryString;
