42df4ee29737c0f8ad11ab11a4049aaa3570fe1a
sedazam <sedazam@gmail.com>
2025-07-05 06:06:53 +0100

    modified: removed only from test.only   parse-query-string.test.js

--- parse-query-string.test.js
+++ parse-query-string.test.js
@@ -1,6 +1,6 @@
 const parseQueryString = require("./parse-query-string");
 describe("", () => {
-  test.only("given a query string with no query parameters, returns an empty object", function () {
+  test("given a query string with no query parameters, returns an empty object", function () {
     const input = "";
     const currentOutput = parseQueryString(input);
     const targetOutput = {};
